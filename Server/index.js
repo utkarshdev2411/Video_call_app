1890c69ad67c0ea63f3923a4d349287b5c20f4b1
Utkarsh Sharma <utkarshdev2411@gmail.com>
2025-02-20 16:28:25 +0530

    Add Socket.IO integration for real-time communication

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,29 @@
+require('dotenv').config();
+require('./config/db.js'); 
+const userRoutes = require('./Routes/user.routes'); // Import the user routes
+
+
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
-require('dotenv').config();
-require('./config/db.js'); 
 
-const userRoutes = require('./Routes/user.routes'); // Import the user routes
+const http = require('http');
+
+const socketIo = require('socket.io');
+const server=http.createServer(app);
+const io =  socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
